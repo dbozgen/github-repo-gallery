840e3dd258a7022177eaa44466b7197a54d5b76e
dogukan_o <dogukan.ozgen@gmail.com>
2024-06-07 23:19:13 -0700

    added API

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const profile = document.querySelector(".overview");
+const username = "dbozgen"; 
+
+const gitProfile = async function(){
+    const responseData = await fetch (`https://api.github.com/users/${username}`);
+    const data = await responseData.json();
+    DisplayUserInfo(data);
+};
+
+gitProfile();
+
+const DisplayUserInfo  = function(data){
+    const div = document.createElement("div");
+    div.classList.add("user-info");
+    div.innerHTML =`
+        <figure>
+      <img alt="user avatar" src=${data.avatar_url} />
+    </figure>
+    <div>
+      <p><strong>Name:</strong> ${data.name}</p>
+      <p><strong>Bio:</strong> ${data.bio}</p>
+      <p><strong>Location:</strong> ${data.location}</p>
+      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div>`; 
+    profile.append(div);
+};
