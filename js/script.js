7c00983cd8fabf6f7cd8b8e3852e71b001bf1f3e
dogukan_o <dogukan.ozgen@gmail.com>
2024-06-10 18:29:00 -0700

    added repo display function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const profile = document.querySelector(".overview");
 const username = "dbozgen"; 
+const repoList = document.querySelector(".repo-list");
 
 const gitProfile = async function(){
     const responseData = await fetch (`https://api.github.com/users/${username}`);
@@ -23,13 +24,21 @@ const DisplayUserInfo  = function(data){
       <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
     </div>`; 
     profile.append(div);
-    displayRepos();
+    readyRepos();
 };
 
-const displayRepos = async function (repos) {
+const readyRepos = async function (repos) {
   const selectedRepos = await fetch (`https://api.github.com/users/${username}/repos?sort=update&per_page=100`);
   const repoData = await selectedRepos.json();
-  displayRepos(repoData);
-
-
+  displayRepos (repoData);
 };
+
+const displayRepos = function (repos) {
+  for (const repo of repos) {
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`; 
+    repoList.append(repoItem);
+  }
+};
+
